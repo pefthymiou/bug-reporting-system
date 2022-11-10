196e194ce1f0c2254144861b515985c417894137
Panagiotis Efthymiou <panefthymiou@outlook.com>
2022-11-10 19:00:48 +0200

    Bugs List changes

--- src/app/main/bug-list/bug-list.component.ts
+++ src/app/main/bug-list/bug-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Bug } from 'src/app/models/bug';
+import { BugsService } from 'src/app/services/bugs.service';
 
 @Component({
   selector: 'app-bug-list',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bug-list.component.css']
 })
 export class BugListComponent implements OnInit {
+  bugs: Bug[] = [];
 
-  constructor() { }
+  constructor(private service: BugsService, private router: Router) { }
 
   ngOnInit(): void {
+    this.service.getBugs().subscribe(bugList => this.bugs = bugList);
   }
 
+  createNewBug(): void {
+    this.router.navigate(['/bugs']);
+  }
+
+  bugListExist(): boolean {
+    return this.bugs !== null;
+  }
+
+  editBug(bugId: string): void {
+    this.router.navigate(['/bugs', bugId]);
+  }
 }
